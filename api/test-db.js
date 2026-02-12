cbbe05e51562b5c6a6e6b21338a77737f2a8e522
jtstroup <joshua.t.stroup@gmail.com>
2026-02-11 19:43:11 -0700

    use separate env vars instead of connection url

--- api/test-db.js
+++ api/test-db.js
@@ -6,7 +6,11 @@ export default async function handler(req, res) {
   }
 
   const client = new Client({
-    connectionString: process.env.DATABASE_URL
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
 
   try {
